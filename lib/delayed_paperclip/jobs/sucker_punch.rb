c81743c5b695fe68894c91257eeaed758b78e7c4
Jonathan Linowes <jonathan@parkerhill.com>
2014-07-26 22:53:04 -0400

    perform sucker punch

--- lib/delayed_paperclip/jobs/sucker_punch.rb
+++ lib/delayed_paperclip/jobs/sucker_punch.rb
@@ -2,21 +2,38 @@ require 'sucker_punch'
 
 module DelayedPaperclip
   module Jobs
-    class SuckerPunch
+    class SuckerPunch < Struct.new(:instance_klass, :instance_id, :attachment_name)
       include ::SuckerPunch::Job
-      @queue = :paperclip
 
       def self.enqueue_delayed_paperclip(instance_klass, instance_id, attachment_name)
-        byebug
-        ::SuckerPunch.new.async.perform(instance_klass, instance_id, attachment_name)
+        # debugger
+        Rails.logger.ino "@@@  @@@  @@@ enqueue_delayed_paperclip : #{instance_id} @@@ @@@ @@@"
+        new.async.perform(instance_klass, instance_id, attachment_name)
       end
-      
-      def self.perform(instance_klass, instance_id, attachment_name)
-        byebug
+
+      def perform(instance_klass, instance_id, attachment_name)
+        # debugger
+        Rails.logger.info "@@@@@@@@@@@@@@@@@@ In perform #{instance_id}"
         ActiveRecord::Base.connection_pool.with_connection do 
           DelayedPaperclip.process_job(instance_klass, instance_id, attachment_name)
         end
+        Rails.logger.ino "leaving perform @@@@@@@@@@@@@@@@"
       end
     end
+    # class SuckerPunch
+    #   include ::SuckerPunch::Job
+    #   @queue = :paperclip
+
+    #   def self.enqueue_delayed_paperclip(instance_klass, instance_id, attachment_name)
+    #     debugger
+    #     ::SuckerPunch.new.async.perform(instance_klass, instance_id, attachment_name)
+    #   end
+    #   def perform(instance_klass, instance_id, attachment_name)
+    #     debugger
+    #     ActiveRecord::Base.connection_pool.with_connection do 
+    #       DelayedPaperclip.process_job(instance_klass, instance_id, attachment_name)
+    #     end
+    #   end
+    # end
   end
 end
